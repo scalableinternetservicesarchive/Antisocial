61b3d98c7e1f3d944b4a1f5be97fab5d01dedc35
avanitanna <avanina001@gmail.com>
2021-10-29 16:01:45 -0700

    create basic posts features

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_27_020820) do
+ActiveRecord::Schema.define(version: 2021_10_29_221909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "invitations", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.integer "friend_id"
-    t.boolean "confirmed", default: false
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_invitations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,5 +35,4 @@ ActiveRecord::Schema.define(version: 2021_10_27_020820) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "invitations", "users"
 end

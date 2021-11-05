92bc81f1167e356304bdb040ce766f2f96ae8a0f
nvmb3r <eljon@ucsb.edu>
2021-11-04 19:01:46 -0700

    Implemented rudimental version of friendship/follow relation

--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
+  resources :friendships
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_30_202007) do
+ActiveRecord::Schema.define(version: 2021_11_05_011903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2021_10_30_202007) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "invitations", force: :cascade do |t|

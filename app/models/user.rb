8a4fd4a44d35e8a65c5d09d5a33b82ae25b5250d
Saastha <saasthavasan@gmail.com>
2021-11-18 19:59:27 -0800

    Added relation for user post and comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
   has_many :friendships
   has_many :friends, :through => :friendships
   has_many :posts, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,6 @@ Rails.application.routes.draw do
       get :posts
     end
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
